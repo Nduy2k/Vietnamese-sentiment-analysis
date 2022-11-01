c55c819d1fcb88c5e6dcc5e41703105effa73dd2
Nduy2k <nvh.duy20@gmail.com>
2022-11-01 18:20:29 +0700

    get ratings data from shopee

--- get_ratings.py
+++ get_ratings.py
@@ -1,36 +1,23 @@
-import re
-import requests
-import json
-import pickle
-import yaml
 import pandas as pd
+import torch
+from model import load_bert, ClassifierModel
+from utils import get_ratings
+from predict import pred_df
 
-# Header will be deleted
-HEADER = {
-    "color": "màu sắc",
-    "material": "chất liệu",
-    "ship": "giao",
-    "describe": "đúng với mô tả",
-    "coin": "nhận xu"
-}
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Number of rating will be got
-MAX_RATINGS = 200
+tokenizer, phoBert = load_bert()
+model = ClassifierModel(phoBert)
+model = torch.load("model/model.pt", map_location=torch.device(device))
 
-# Shopee API - Get Ratings
-API_URL = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid=" \
-          "{item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
+url = "https://shopee.vn/Gi%C3%A0y-n%E1%BB%AF-gi%C3%A0y-da-n%E1%BB%AF-c%E1%BB%95-th%E1%BA%A5p-%C4%91%E1%BA%BF-%C4%91%E1%BB%99n-gi%E1%BA%A7y-th%E1%BB%9Di-trang-ki%E1%BB%83u-d%C3%A1ng-basic-ch%E1%BA%A5t-li%E1%BB%87u-da-PU-%C4%91%E1%BA%BF-cao-su-%C4%91%E1%BB%99n-4cm-L%C3%B9-store-GD02-i.404405029.20244927353?sp_atk=cb3eea42-18f8-4fac-b484-1263b4c9a7a0&xptdk=cb3eea42-18f8-4fac-b484-1263b4c9a7a0"
 
-# Init some path will be used
-tokenize_path = "model/tokenizer.pkl"
+_, data, _ = get_ratings(url)
+predicted_data = pred_df(data, model, tokenizer)
 
-# Load dictionary file
-with open(tokenize_path, 'rb') as f:
-    dictionary = pickle.load(f)
-    f.close()
+df = pd.DataFrame(predicted_data)
 
-with open("utils.yaml", 'r') as yaml_file:
-    data = yaml.load(yaml_file, Loader=yaml.FullLoader)
-    yaml_file.close()
-header = data['HEADER']
-print(header)
+file_path = "data/data1.csv"
+df.to_csv(file_path, mode='a', index=False, header=False, encoding="utf-8")
+
+print(predicted_data)
